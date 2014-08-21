74fa916be4c461dd29784434c0dc29483edb4c17
Ivan Wills <ivan.wills@gmail.com>
2014-08-21 21:25:34 +1000

    Updating for release of version 0.5.1

--- lib/VCS/Which/Plugin.pm
+++ lib/VCS/Which/Plugin.pm
@@ -15,7 +15,7 @@ use English qw/ -no_match_vars /;
 use base qw/Exporter/;
 use File::chdir;
 
-our $VERSION     = version->new('0.5.0');
+our $VERSION     = version->new('0.5.1');
 our @EXPORT_OK   = qw//;
 our %EXPORT_TAGS = ();
 
@@ -131,7 +131,7 @@ VCS::Which::Plugin - Base class for the various VCS plugins
 
 =head1 VERSION
 
-This documentation refers to VCS::Which::Plugin version 0.5.0.
+This documentation refers to VCS::Which::Plugin version 0.5.1.
 
 =head1 SYNOPSIS
 
